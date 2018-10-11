d68f5381a959a7574d2433408a8854eb52eb7fbc
ce691 <email@email.com>
2018-10-11 10:35:55 -0500

    work with weather api

--- ShotGunGuide.py
+++ ShotGunGuide.py
@@ -10,9 +10,17 @@ def home():
 
 @app.route('/city')
 def get_city():
+    # get the user entry from the form
     zip = request.args.get('city')
+    # get city and weather info from weather api
     city = weather_api.get_city(URL_TEMPLATE_BY_ZIP.format(CITYZIP=zip))
-    return render_template('city.html', city=city)
+    w = weather_api.get_weather(URL_TEMPLATE_BY_ZIP.format(CITYZIP=zip))
+    # set the variables that the form needs from the weather dictionary
+    key = w['key']
+    description = w['description']
+    temp = w['temp']
+
+    return render_template('city.html', city=city, key=key, description=description, temp=temp)
 
 
 if __name__ == '__main__':
--- apis/weather_api.py
+++ apis/weather_api.py
@@ -10,5 +10,26 @@ mpls_url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8faf
 def get_city(url):
     response = requests.get(url).json()
     return response['city']['name']
-city = get_city(URL_TEMPLATE_BY_ZIP.format(CITYZIP=33101))
-print(city)
+def get_weather(url):
+    response = requests.get(url).json()
+    weather_list = response['list']
+    weather_obj = weather_list[0]
+    temp = convert_to_fahrenheit(weather_obj['main']['temp'])
+    temp_min = convert_to_fahrenheit(weather_obj['main']['temp_min'])
+    temp_max =  convert_to_fahrenheit(weather_obj['main']['temp_max'])
+    weather_main_desc = weather_obj['weather'][0]['main']
+    weather_desc = weather_obj['weather'][0]['description']
+    weather_dict = {'key' : weather_main_desc, 'description' : weather_desc, 'temp' : round(temp), 'max': round(temp_max), 'min': round(temp_min) }
+    return weather_dict
+def convert_to_fahrenheit(kel):
+    return (1.8 * (kel - 273)) + 32
+
+### TEST CODE ###
+def print_current_weather(city, w):
+    #city = get_city(URL_TEMPLATE_BY_ZIP.format(CITYZIP=57101))
+    #w = get_weather(URL_TEMPLATE_BY_ZIP.format(CITYZIP=57101))
+    print('Current weather in ' + city)
+    print(w['key'] + ': ' + w['description'])
+    print('Current temp is ' + str(w['temp']))
+    print('Today\'s max temp: ' + str(w['max']) + ', min temp: ' + str(w['min']))
+
