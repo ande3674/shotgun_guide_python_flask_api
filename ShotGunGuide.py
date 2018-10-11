5ab189ff5c4e85b19b823919b5aed456b2782c2a
ce691 <email@email.com>
2018-10-11 12:31:02 -0500

    Add more to the homepage form

--- ShotGunGuide.py
+++ ShotGunGuide.py
@@ -11,16 +11,23 @@ def home():
 @app.route('/city')
 def get_city():
     # get the user entry from the form
-    zip = request.args.get('city')
+    zip1 = request.args.get('origin')
+    zip2 = request.args.get('destination')
     # get city and weather info from weather api
-    city = weather_api.get_city(URL_TEMPLATE_BY_ZIP.format(CITYZIP=zip))
-    w = weather_api.get_weather(URL_TEMPLATE_BY_ZIP.format(CITYZIP=zip))
+    city1 = weather_api.get_city(URL_TEMPLATE_BY_ZIP.format(CITYZIP=zip1))
+    w1 = weather_api.get_weather(URL_TEMPLATE_BY_ZIP.format(CITYZIP=zip1))
+    city2 = weather_api.get_city(URL_TEMPLATE_BY_ZIP.format(CITYZIP=zip2))
+    w2 = weather_api.get_weather(URL_TEMPLATE_BY_ZIP.format(CITYZIP=zip2))
     # set the variables that the form needs from the weather dictionary
-    key = w['key']
-    description = w['description']
-    temp = w['temp']
+    key1 = w1['key']
+    description1 = w1['description']
+    temp1 = w1['temp']
+    key2 = w2['key']
+    description2 = w2['description']
+    temp2 = w2['temp']
 
-    return render_template('city.html', city=city, key=key, description=description, temp=temp)
+    return render_template('city.html', city1=city1, key1=key1, description1=description1, temp1=temp1,
+                           city2=city2, key2=key2, description2=description2, temp2=temp2)
 
 
 if __name__ == '__main__':
--- apis/weather_api.py
+++ apis/weather_api.py
@@ -1,6 +1,5 @@
 import requests
 # OpenWeatherMap API
-
 KEY = '8faf8df6a0906553e13e8a6689203f78'
 URL_TEMPLATE_BY_ID = 'http://api.openweathermap.org/data/2.5/forecast?id={CITYID}&APPID=8faf8df6a0906553e13e8a6689203f78'
 URL_TEMPLATE_BY_ZIP = 'http://api.openweathermap.org/data/2.5/forecast?zip={CITYZIP}&APPID=8faf8df6a0906553e13e8a6689203f78'
