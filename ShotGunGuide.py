38631b8e08fbbe3e481eecfb898536b6f66b8d58
ce691 <email@email.com>
2018-10-12 23:53:42 -0500

    add a midway stop (not fully working as expected)

--- ShotGunGuide.py
+++ ShotGunGuide.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
 from apis import weather_api, maps_api
+from googlemaps.client import geocode, reverse_geocode
+#from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+#bootstrap = Bootstrap(app)
 URL_TEMPLATE_BY_ZIP = 'http://api.openweathermap.org/data/2.5/forecast?zip={CITYZIP}&APPID=8faf8df6a0906553e13e8a6689203f78'
 
 @app.route('/')
@@ -10,15 +13,36 @@ def home():
 
 @app.route('/trip')
 def get_trip():
+    # Get info on the origin & destination locations: name, longitude and latitude
     origin = request.args.get('origin')
+    #origin_lon, origin_lat = weather_api.get_lon_lat_for_place(origin)
+    #origin_ditc = {'name':origin, 'lon':origin_lon, 'lat':origin_lat}
     dest = request.args.get('destination')
+    #dest_lon, dest_lat = weather_api.get_lon_lat_for_place(dest)
+    #dest_ditc = {'name': dest, 'lon': dest_lon, 'lat': dest_lat}
+
+    # get weather info for these places
     ostatus1, ostatus2, otemp = weather_api.get_statuses_and_temp_at_place(origin)
     dstatus1, dstatus2, dtemp = weather_api.get_statuses_and_temp_at_place(dest)
+    # get a map for these places
     ourl = maps_api.get_static_map_of_place(origin)
     durl = maps_api.get_static_map_of_place(dest)
-    return render_template('trip.html', city1=origin, key1=ostatus1, description1=ostatus2, temp1=otemp, url1=ourl,
-                           city2=dest, key2=dstatus1, description2=dstatus2, temp2=dtemp, url2=durl)
 
+    # get info about a point along the way
+    midway_step = maps_api.get_directions(origin, dest)
+    midway_lon, midway_lat = maps_api.get_midway_location(midway_step)
+    # get the closest city to this place! TODO
+    mplace = maps_api.reverse_geocode_place(midway_lon, midway_lat)
+    mplace_name = mplace[0]['formatted_address']#['formatted_address']
+    mstatus1, mstatus2, mtemp = weather_api.get_statuses_and_temp_at_coords(midway_lon, midway_lat)
+    murl = maps_api.get_static_map_of_coords(midway_lon, midway_lat)
+
+    return render_template('trip.html', city1=origin, key1=ostatus1, description1=ostatus2, temp1=otemp, url1=ourl,
+                           city2=dest, key2=dstatus1, description2=dstatus2, temp2=dtemp, url2=durl,
+                           city3=mplace_name, key3=mstatus1, description3=mstatus2, temp3=mtemp, url3=murl)
+
+
+### This is just for testing - delete later ###
 @app.route('/city')
 def get_city():
     # get the user entry from the form
--- apis/maps_api.py
+++ apis/maps_api.py
@@ -1,10 +1,9 @@
-import requests
+import requests, googlemaps
+from googlemaps.client import geocode, reverse_geocode#, MANY MORE WILL BE USEFUL!!!!!!!
+from datetime import datetime
+
 # Google Maps API
 GOOGLE_KEY = 'AIzaSyCUzzNpRfSkRPbdOHtrjnlfCaCZ26cNKnc'
-# MapQuest API
-MAPQUEST_KEY = '8ET6X9Q2Uwf6TCAQHk5FNeDrjT1SC5Id'
-MAPQUEST_SECRET = 'BAIytvgECoeYE8EK'
-
 DIRECTIONS_URL_TEMPLATE = 'https://maps.googleapis.com/maps/api/directions/json?origin={ORIGIN}' \
                          '&destination={DESTINATION}&key=AIzaSyCUzzNpRfSkRPbdOHtrjnlfCaCZ26cNKnc'
 STATIC_MAP_URL = 'https://maps.googleapis.com/maps/api/staticmap?center={CENTER}' \
@@ -13,24 +12,23 @@ DIRECTIONS_URL_EXAMPLE1 = 'https://maps.googleapis.com/maps/api/directions/json?
                          '&destination=25.7617,80.1918&key=AIzaSyCUzzNpRfSkRPbdOHtrjnlfCaCZ26cNKnc'
 DIRECTIONS_URL_EXAMPLE2 = 'https://maps.googleapis.com/maps/api/directions/json?origin=minneapolis&destination=miami&key=AIzaSyCUzzNpRfSkRPbdOHtrjnlfCaCZ26cNKnc'
 
-#https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyCUzzNpRfSkRPbdOHtrjnlfCaCZ26cNKnc
+gmaps = googlemaps.Client(key='AIzaSyCUzzNpRfSkRPbdOHtrjnlfCaCZ26cNKnc')
+
+
+def geocode_place(place):
+    return geocode(gmaps, place)
+def reverse_geocode_place(lon, lat):
+    return reverse_geocode(gmaps, (lat, lon))
+
 def get_static_map_of_place(place):
     place = split_up(place)
     url = STATIC_MAP_URL.format(CENTER=place, ZOOM=10, TYPE='roadmap')
-    #response = requests.get(url)
     return url
-
-def split_up(s):
-    split = s.split(" ")
-    return_string = ''
-    for i in range(len(split)):
-        if i == len(split)-1:
-            return_string += split[i]
-        else:
-            return_string += (split[i] + '+')
-    return return_string
-
-def get_directions(url):
+def get_static_map_of_coords(lon, lat):
+    url = STATIC_MAP_URL.format(CENTER=str(lon)+','+str(lat), ZOOM=10, TYPE='roadmap')
+    return url
+def get_directions(place1, place2):
+    url = DIRECTIONS_URL_TEMPLATE.format(ORIGIN=place1, DESTINATION=place2)
     response = requests.get(url).json()
     routes = response['routes'][0]
     legs = routes['legs'][0]
@@ -43,7 +41,31 @@ def get_midway_location(steps):
     long = location_data['end_location']['lng']
     return lat, long
 
+def split_up(s):
+    split = s.split(" ")
+    return_string = ''
+    for i in range(len(split)):
+        if i == len(split)-1:
+            return_string += split[i]
+        else:
+            return_string += (split[i] + '+')
+    return return_string
 
-s = get_directions(DIRECTIONS_URL_EXAMPLE2)
-lat, long = get_midway_location(s)
-print('lat: ' + str(lat) + ", long: " + str(long))
+def get_directions1(url):
+    response = requests.get(url).json()
+    routes = response['routes'][0]
+    legs = routes['legs'][0]
+    steps = legs['steps']
+    return steps
+
+##############
+# TEST STUFF #
+##############
+
+#s = get_directions(DIRECTIONS_URL_EXAMPLE2)
+#lat, long = get_midway_location(s)
+#print('lat: ' + str(lat) + ", long: " + str(long))
+#gp = geocode_place('Paris, France')
+#print(gp)
+#p = reverse_geocode_place(gp)
+#print(p)
