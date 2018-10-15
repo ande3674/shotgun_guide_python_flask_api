18cbc6d07b7ae8fe04c1b6c8402b93530603416b
ce691 <email@email.com>
2018-10-15 17:16:40 -0500

    add some error handling

--- apis/places_api.py
+++ apis/places_api.py
@@ -5,15 +5,22 @@ GOOGLE_KEY = 'AIzaSyCUzzNpRfSkRPbdOHtrjnlfCaCZ26cNKnc'
 PLACE_TYPE_LIST = [ 'restaurant', 'zoo', 'park', 'shoppng_mall', 'museum' ]
 PLACE_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={LATLON}&radius=3000' \
                    '&type={TYPE}&keyword={KEY}&key=AIzaSyCUzzNpRfSkRPbdOHtrjnlfCaCZ26cNKnc'
+PLACE_SEARCH_URL1 = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={LATLON}&radius=3000' \
+                   '&type={TYPE}&key=AIzaSyCUzzNpRfSkRPbdOHtrjnlfCaCZ26cNKnc'
 
 def get_place_name(lat, lon):
-    r = random.randint(0, len(PLACE_TYPE_LIST)-1)
-    type = PLACE_TYPE_LIST[0]
-    url = PLACE_SEARCH_URL.format(LATLON=format_lat_lon(lat, lon), TYPE=type, KEY='popular')
-    response = requests.get(url).json()
-    result = response['results'][0]
-    name = result['name']
-    return name
+    try:
+        r = random.randint(0, len(PLACE_TYPE_LIST)-1)
+        type = PLACE_TYPE_LIST[0]
+        #url = PLACE_SEARCH_URL.format(LATLON=format_lat_lon(lat, lon), TYPE=type, KEY='popular')
+        url = PLACE_SEARCH_URL1.format(LATLON=format_lat_lon(lat, lon), TYPE=type)
+        response = requests.get(url).json()
+        result = response['results'][0]
+        name = result['name']
+        return name
+    except IndexError:
+        return 'No suggestion for this area'
+
 
 def format_lat_lon(lat, lon):
     return str(lat) + "," + str(lon)
